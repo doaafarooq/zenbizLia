c7d1b3bbca0eb24cb82a442f1b4dfc2918ea145c
ElvinSalim1 <elvinsalim5@gmail.com>
2020-10-14 11:45:16 +0200

    new

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <Carousel />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '../components/Navbar'
+import Carousel from '../components/Carousel'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+   Navbar,
+   Carousel
   }
 }
 </script>
