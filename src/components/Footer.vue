eb17b635a03b8bfe24411a3694be0086c16fb8d5
doaafarooq <56040568+doaafarooq@users.noreply.github.com>
2020-10-15 09:31:09 +0200

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row row-cols-2">
+      <div class="col">KONTAKTA OSS</div>
+      <div class="col">021-12 80 00</div>
+      <div class="col">info@zenbiz.se</div>
+      <div class="col">Fallhammargatan 1 B</div>
+      <div class="col">721 33 Västerås</div>
+       <div class="col">VAR SOCIAL</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+
+
+</style>
