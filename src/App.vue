883a81f4ffa7192b3ce368993f396895e587f775
ElvinSalim1 <elvinsalim5@gmail.com>
2020-10-15 09:53:48 +0200

    app.vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-   
-=======
->>>>>>> c7d1b3bbca0eb24cb82a442f1b4dfc2918ea145c
-    <router-view/>
-   
+    <router-view/>  
   </div>
 </template>
 <script>
 
-<<<<<<< HEAD
-
 export default {
    name: "App",
   components: {
-  
-   
+     
   },
 }
 </script>
 <style>
-
-=======
-<style>
 body{
   padding-right: 0px !important;
 }
->>>>>>> c7d1b3bbca0eb24cb82a442f1b4dfc2918ea145c
+
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,20 +4,75 @@
             <b-navbar-brand href="#">
                 <i class="logo">Zenbiz</i>
             </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                        <b-button
+                            @click="modaldata2('Check Reservation', 'success')"
+                            size="sm"
+                            class="my-2 mr-2 my-sm-0"
+                        >
+                            <i class="fas fa-door-open"></i>Check Reservation
+                        </b-button>
+                        <b-button
+                            @click="modaldata2('Cancel Reservation', 'danger')"
+                            size="sm"
+                            class="my-2 mr-2 my-sm-0"
+                        >
+                            <i class="fas fa-window-close"></i>Cancel Reservation
+                        </b-button>
+                        <b-button
+                            @click="modaldata2('Contact', 'primary')"
+                            size="sm"
+                            class="my-2 mr-2 my-sm-0"
+                        >
+                            <i class="fas fa-phone"></i>Contact
+                        </b-button>
+                        <b-button
+                            @click="modaldata2('Administrator', 'dark')"
+                            size="sm"
+                            class="my-2 mr-2 my-sm-0"
+                        >
+                            <i class="fas fa-user"></i>Administrator
+                        </b-button>
+                    </b-nav-form>
+                </b-navbar-nav>
+            </b-collapse>
         </b-navbar>
-        <b-navbar-toggle id="nav-collapse">
-        </b-navbar-toggle>
+        <b-modal hide-footer id="my-modal2" :title="title">
+            <b-form @submit="onSubmit2">
+                <b-form-group
+                    v-if="color != 'primary' && color != 'dark'"
+                    id="input-group-1"
+                    label="Reservation ID:"
+                    label-for="input-1"
+                >
+                    <b-form-input
+                        v-if="color != 'primary' && color != 'dark'"
+                        id="input-1"
+                        v-model="form.reservationid"
+                        type="text"
+                        required
+                        placeholder="Enter Reservation ID"
+                    ></b-form-input>
+                </b-form-group>
+                <b-form-group
+                    v-if="color != 'primary' && color != 'dark'"
+                    id="input-group-3"
+                    label="Service:"
+                    label-for="input-3"
+                >
+                    <b-form-select id="input-3" v-model="form.service" :options="services" required>
+                    </b-form-select>
+                </b-form-group>
+                <!--buradan baslayacan emaille -->
+            </b-form>
 
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-form>
-                    <b-button>
-
-                    </b-button>
-                </b-nav-form>
-            </b-navbar-nav>
-            
-        </b-collapse>
+        </b-modal>
     </div>
 </template>
 
@@ -65,7 +120,7 @@ export default {
             this.show=false;
             evt.preventDefault();
             switch(this.title){
-                case 'Avboka bokningen': 
+                case 'Cancel Reservation': 
                     db.collection(this.form.service)
                         .doc(this.form.reservationid)
                         .delete()
@@ -105,10 +160,54 @@ export default {
                     }
                     break;
                 default:
-                    
+                    db.collection(this.form.service)
+                        .doc(this.form.reservationid)
+                        .get()
+                        .then(doc => {
+                            if(doc.exists){
+                                db.collection('clients')
+                                    .doc(doc.data().clientID)
+                                    .get()
+                                    .then(doc2 =>{
+                                        this.showinfo(doc,doc2)
+                                    })
+                            }else{
+                                console.log('error')
+                            }
+                        })
+                        .catch(()=>{
+                            console.log('error')
+                        });
+                    break;
             }
+        },
+        modaldata2(title,color){
+            this.$bvModal.show("my-modal2");
+            this.color = color;
+            this.title = title;
+            this.form.reservationid = "";
+            this.form.service = null;
+            this.form.message = "";
+            this.form.email = "";
+            this.reservation.id = "";
+            this.reservation.name = "";
+            this.reservation.email = "";
+            this.reservation.day = "";
+            this.reservation.price = "";
+            this.admininput.user = '';
+            this.admininput.pass = '';
+            this.show = false;
+
+        },
+        showinfo(doc,doc2){
+            this.show = true;
+            this.reservation.id = doc.id;
+            this.reservation.name = doc2.data().name;
+            this.reservation.email = doc2.data().email;
+            this.reservation.day = doc.data().day;
+            this.reservation.price = doc.data().price;
         }
-    },
+    }
 }
 </script>
 
