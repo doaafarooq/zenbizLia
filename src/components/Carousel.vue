6c46eac7a381530888356c677780132bb3486aa2
atwehalaa <atwehalaa@gmail.com>
2020-10-21 09:21:28 +0200

    fix

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -8,12 +8,12 @@
       <img class="d-block w-100 images" src="../assets/carousel2.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
+      
       <img class="d-block w-100 images" src="../assets/carousel3.jpg" alt="Third slide">
     </div>
   </div>
 </div>
 </template>
-
 <script>
 export default {
     name: 'Carousel'
@@ -21,11 +21,10 @@ export default {
 </script>
 
 <style>
-<<<<<<< HEAD
+
 #imageca{
     height: 70vh !important;
 }
-=======
 
->>>>>>> 6b4ebc00a9d3f58f3fefbad9210f5e3c6331779e
+
 </style>
