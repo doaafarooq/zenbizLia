6d63a82c0227c2967697b0136c351a3f4f18d697
ElvinSalim1 <elvinsalim5@gmail.com>
2020-11-06 09:11:03 +0100

    design of admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -30,52 +30,74 @@
     <todo />
 
     <b-container fluid class="bv-example-row mt-4">
-       
-           <div class="py-2" >
-                                <h2 class="text-white">Programmering</h2>
-                                
-                                <b-table :fields="fields" bordered striped hover :items="Programmering" class="text-white">
-                                    <template v-slot:cell(delete)= "row"> 
-                                        <b-button size ="md" variant ="danger" block @click="deletekey('Programmering', row.item.id)">delete
-                                        </b-button>
-                                    </template>
-
-                                </b-table>
-            </div>
-            <div class="py-2" >
-                                <h2 >IT SÄKERHET</h2>
-                                
-                                <b-table :fields="fields" bordered striped hover :items="ITSäkerhet" class="text-white">
-                                    <template v-slot:cell(delete)= "row"> 
-                                        <b-button size ="md" variant ="danger" block @click="deletekey('ITSäkerhet', row.item.id)">delete
-                                        </b-button>
-                                    </template>
-
-                                </b-table>
-            </div>
-            <div class="py-2" >
-                                <h2 class="text-white">Redovisning</h2>
-                                
-                                <b-table :fields="fields" bordered striped hover :items="Redovisning" class="text-white">
-                                    <template v-slot:cell(delete)= "row"> 
-                                        <b-button size ="md" variant ="danger" block @click="deletekey('Redovisning', row.item.id)">delete
-                                        </b-button>
-                                    </template>
-
-                                </b-table>
-            </div>
-             <div>
-                                <h2 >clients</h2>
-                                
-                                <b-table :fields="fields" bordered striped hover :items="clients" class="text-white">
-                                    <template v-slot:cell(delete)= "row"> 
-                                        <b-button size ="md" variant ="danger" block @click="deletekey('clients', row.item.id)">delete
-                                        </b-button>
-                                    </template>
-
-                                </b-table>
-            </div>
-          
+        <div>
+           <b-col cols="12" >
+                <h2 class="text-white">Programmering</h2>
+                <b-img :src="require('../assets/programmering.jpg')" fluid alt="Responsive image"></b-img>
+                <b-table :fields="fields" bordered striped hover :items="programmering" class="text-white">
+                    <template  v-slot:cell(delete)= "row"> 
+                        <b-button size ="md" variant ="danger" block @click="deletekey('Programmering',
+                         row.item.id)">Radera Bokningen
+                        </b-button>
+                    </template>
+                </b-table>
+            </b-col>
+            <b-col cols="12" >
+                <h2 >IT SÄKERHET</h2>
+                <b-img :src="require('../assets/itsäkerhet.jpg')" fluid alt="Responsive image"></b-img>
+                <b-table :fields="fields" bordered striped hover :items="itsäkerhet" class="text-white">
+                    <template v-slot:cell(delete)= "row"> 
+                        <b-button size ="md" variant ="danger" block @click="deletekey('ITSäkerhet',
+                        row.item.id)">Radera Bokningen
+                        </b-button>
+                    </template>
+                </b-table>
+            </b-col>
+            <b-col cols="12" >
+                <h2 class="text-white">Redovisning</h2>
+                <b-img :src="require('../assets/redovisning.jpg')" fluid alt="Responsive image"></b-img>
+                <b-table :fields="fields" bordered striped hover :items="redovisning" class="text-white">
+                    <template v-slot:cell(delete)= "row"> 
+                        <b-button size ="md" variant ="danger" block @click="deletekey('Redovisning',
+                         row.item.id)">Radera Bokningen
+                        </b-button>
+                    </template>
+                </b-table>
+            </b-col>
+            <b-col cols="12" >
+                <h2 class="text-white">IT Servis</h2>
+                <b-img :src="require('../assets/itservis.jpg')" fluid alt="Responsive image"></b-img>
+                <b-table :fields="fields" bordered striped hover :items="itservis" class="text-white">
+                    <template v-slot:cell(delete)= "row"> 
+                        <b-button size ="md" variant ="danger" block @click="deletekey('ITServis',
+                         row.item.id)">Radera Bokningen
+                        </b-button>
+                    </template>
+                </b-table>
+            </b-col>
+            <b-col cols="12">
+                <h2 >Kunder</h2>
+                
+                <b-table :fields="fieldsClient" bordered striped hover :items="clients" class="text-white">
+                    <template v-slot:cell(delete)= "row"> 
+                        <b-button size ="md" variant ="danger" block @click="deletekey('clients',
+                         row.item.id)">Radera Kunder
+                        </b-button>
+                    </template>
+                </b-table>
+            </b-col>
+            <b-col cols="12" >
+                <h2 class="text-white">Kontakta Oss</h2>
+                
+                <b-table :fields="fieldsMessage" bordered striped hover :items="messages" class="text-white">
+                    <template v-slot:cell(delete)= "row"> 
+                        <b-button size ="md" variant ="danger" block @click="deletekey('messages',
+                         row.item.id)">Radera Meddelande
+                        </b-button>
+                    </template>
+                </b-table>
+            </b-col>
+        </div> 
     </b-container>
   
 </div>
@@ -92,60 +114,70 @@ export default {
 
 data(){
     return{ 
-      Programmering:[],
-        ITServis : [],
-        ITSäkerhet:[],
-        Redovisning:[],
+      programmering:[],
+        itservis : [],
+        itsäkerhet:[],
+        redovisning:[],
         clients:[],
         messages:[],
         fields:[
             {
                 key:'id',
-                lablel:'Id',
+                label:'Id',
                 sortable:true
             },
              {
                 key:'clientID',
-                lablel:'client ID',
+                label:'Client ID',
                 sortable:true
             },
               {
                 key:'name',
-                lablel:'name',
+                label:'name',
                 sortable:true
             },
               {
                 key:'email',
-                lablel:'email',
+                label:'email',
+                sortable:true
+            },
+            {
+                key:'price',
+                label:'Price',
+                sortable:true
+            },
+            {
+                key:'day',
+                label:'Day',
                 sortable:true
             },
                 {
                 key:'delete',
-                lablel:'Delete',
+                label:'Delete',
                 sortable:true
             }
         ],
-        fieldsclient:[
+        fieldsClient:[
                 {
                 key:'id',
-                lablel:'Id',
+                label:'Id',
                 sortable:true
             },
                    {
                 key:'name',
-                lablel:'Name',
+                label:'Name',
                 sortable:true
             },
           
                   {
                 key:'email',
-                lablel:'Email',
+                label:'Email',
                 sortable:true
             },
 
                   {
                 key:'delete',
-                lablel:'Delete',
+                label:'Delete',
                 sortable:true
             },
              
@@ -154,42 +186,53 @@ data(){
         fieldsMessage:[
              {
                 key:'id',
-                lablel:'Id',
+                label:'Id',
                 sortable:true
             },
              {
                 key:'message',
-                lablel:'Message',
+                label:'Message',
                 sortable:true
             },
              {
                 key:'email',
-                lablel:'Email',
+                label:'Email',
                 sortable:true
             },
 
                   {
                 key:'delete',
-                lablel:'Delete',
+                label:'Delete',
                 sortable:true
             },
+            
         ]
     }
 },
 created(){
   db.collection('Programmering')
     .onSnapshot((querySnapshot) => {
-        this.Programmering =[];
+        this.programmering =[];
         querySnapshot.forEach((doc)=>{
             var x= doc.data()
             x.id=doc.id
-            this.Programmering.push(x);
+            this.programmering.push(x);
+        });
+   
+    });
+    db.collection('Redovisning')
+    .onSnapshot((querySnapshot) => {
+        this.redovisning =[];
+        querySnapshot.forEach((doc)=>{
+            var x= doc.data()
+            x.id=doc.id
+            this.redovisning.push(x);
         });
    
     });
     db.collection('clients')
     .onSnapshot((querySnapshot) => {
-        this.Programmering =[];
+        this.clients =[];
         querySnapshot.forEach((doc)=>{
             var x= doc.data()
             x.id=doc.id
@@ -197,23 +240,34 @@ created(){
         });
    
     });
-     db.collection('Redovisning')
+
+     db.collection('messages')
     .onSnapshot((querySnapshot) => {
-        this.Redovisning =[];
+        this.messages =[];
         querySnapshot.forEach((doc)=>{
             var x= doc.data()
             x.id=doc.id
-            this.Redovisning.push(x);
+            this.messages.push(x);
         });
    
     });
     db.collection('ITSäkerhet')
     .onSnapshot((querySnapshot) => {
-        this.ITSäkerhet =[];
+        this.itsäkerhet =[];
         querySnapshot.forEach((doc)=>{
             var x= doc.data()
             x.id=doc.id
-            this.ITSäkerhet.push(x);
+            this.itsäkerhet.push(x);
+        });
+   
+    });
+    db.collection('ITServis')
+    .onSnapshot((querySnapshot) => {
+        this.itservis =[];
+        querySnapshot.forEach((doc)=>{
+            var x= doc.data()
+            x.id=doc.id
+            this.itservis.push(x);
         });
    
     });
@@ -225,7 +279,7 @@ methods:{
             console.log("document is deleted");
         }).catch(function(error){
             console.error("error removing document" , error);
-        })
+        });
     }
 }
 
